bcd4df9e5c431b767af64d356a13d2bd73470d37
AriaFallah <aria@fallah.us>
2018-11-27 03:23:55 -0500

    first commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,88 @@
 import "../assets/layout.css";
 import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 import React from "react";
 import styled from "react-emotion";
 import Helmet from "react-helmet";
 
-const Main = styled.main`
-  margin: 0 auto;
-  max-width: 960px;
-  padding: 15px;
-  height: 100vh;
-`;
+const appleSvg = require("../assets/apple.svg") as string;
+const instagramSvg = require("../assets/instagram.svg") as string;
+const soundcloudSvg = require("../assets/soundcloud.svg") as string;
+const spotifySvg = require("../assets/spotify.svg") as string;
+const youtubeSvg = require("../assets/youtube.svg") as string;
 
 const Nav = styled.nav`
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 15px;
+  background-color: rgb(255, 255, 255);
+  border-bottom: 6px solid #d02120;
 `;
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  max-width: 960px;
+  margin: 0 auto;
+  height: 100%;
+  padding: 10px;
+`;
+
+const LogoContainer = styled.a`
+  display: flex;
+  color: black;
+  align-items: center;
+  text-decoration: none;
+
+  > div {
+    border-radius: 50%;
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    box-shadow: 3px 3px 6px 0px rgba(0, 0, 0, 0.3);
+  }
+
+  > h1 {
+    margin-bottom: 0;
+    margin-left: 10px;
+    margin-right: 20px;
+
+    @media (max-width: 660px) {
+      font-size: 1em;
+    }
+  }
+`;
+
+const SocialLinks = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 460px) {
+    margin-top: 10px;
+  }
+
+  a + a {
+    margin-left: 10px;
+  }
+
+  img {
+    height: 35px;
+  }
+`;
+
+const socialLinks = [
+  {
+    logo: appleSvg,
+    href: "https://itunes.apple.com/us/artist/nuq/1403168719"
+  },
+  { logo: instagramSvg, href: "https://www.instagram.com/sadlilblackboy/" },
+  { logo: soundcloudSvg, href: "https://soundcloud.com/nuq-the-most-dope" },
+  {
+    logo: spotifySvg,
+    href: "https://open.spotify.com/artist/4oQFbIzjeTpjBFjB6Zri2X"
+  },
+  {
+    logo: youtubeSvg,
+    href: "https://www.youtube.com/channel/UCF-q9z4IF8CUWyj1_8hZDtg"
+  }
+];
 
 export const Layout = ({ children }) => (
   <StaticQuery
@@ -26,6 +93,13 @@ export const Layout = ({ children }) => (
             title
           }
         }
+        logo: file(relativePath: { eq: "logo.png" }) {
+          childImageSharp {
+            fixed(width: 50) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
       }
     `}
     render={data => (
@@ -33,13 +107,31 @@ export const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" }
+            {
+              name: "description",
+              content:
+                "Who are you? Why are you here? Lofi hiphop, music, anime, and chill vibes. Lets be sad together."
+            }
           ]}
         >
           <html lang="en" />
         </Helmet>
-        <Main>{children}</Main>
+        <Nav>
+          <NavContainer>
+            <LogoContainer href="/">
+              <Img fixed={data.logo.childImageSharp.fixed} />
+              <h1>SADLILBLACKBOY</h1>
+            </LogoContainer>
+            <SocialLinks>
+              {socialLinks.map(l => (
+                <a key={l.href} href={l.href}>
+                  <img src={l.logo} />
+                </a>
+              ))}
+            </SocialLinks>
+          </NavContainer>
+        </Nav>
+        {children}
       </React.Fragment>
     )}
   />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,45 @@
 import React from "react";
-import { Layout } from "../components/layout";
-import {
-  Apple,
-  Instagram,
-  Logo,
-  SoundCloud,
-  Spotify,
-  Youtube
-} from "../components/images";
 import styled from "react-emotion";
-
-const CenterPiece = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  text-align: center;
-`;
-
-const Social = styled.section`
-  display: flex;
-  margin-top: 1.45rem;
-  a + a {
-    margin-left: 15px;
-  }
-`;
+import { Layout } from "../components/layout";
 
 const Playlist = styled.div`
-  border: none;
-  height: 0;
-  width: 75%;
-  padding-bottom: 42.188%;
+  width: 100%;
   position: relative;
-  margin-top: 1.45rem;
+  height: 0;
+  padding-bottom: 56.25%;
 
   iframe {
     border: none;
     position: absolute;
     top: 0;
+    bottom: 0;
     left: 0;
+    right: 0;
     width: 100%;
     height: 100%;
   }
 `;
 
-const socialLinks = [
-  { logo: <Apple />, href: "" },
-  { logo: <Instagram />, href: "https://www.instagram.com/sadlilblackboy/" },
-  { logo: <SoundCloud />, href: "https://soundcloud.com/nuq-the-most-dope" },
-  {
-    logo: <Spotify />,
-    href: "https://open.spotify.com/artist/4oQFbIzjeTpjBFjB6Zri2X"
-  },
-  {
-    logo: <Youtube />,
-    href: "https://www.youtube.com/channel/UCF-q9z4IF8CUWyj1_8hZDtg"
-  }
-];
+const Content = styled.section`
+  color: white;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 960px;
+  padding-top: 15%;
+`;
 
 export default () => (
   <Layout>
-    <CenterPiece>
-      <h1>SADLILBLACKBOY</h1>
-      <Logo />
-      <Social>
-        {socialLinks.map(l => (
-          <a key={l.href} href={l.href}>
-            {l.logo}
-          </a>
-        ))}
-      </Social>
-      <Playlist>
-        <iframe
-          allow="autoplay"
-          src="https://www.youtube.com/embed?listType=list&list=UUF-q9z4IF8CUWyj1_8hZDtg&autoplay=1"
-        />
-      </Playlist>
-    </CenterPiece>
+    <Playlist>
+      <iframe
+        allow="autoplay"
+        src="https://www.youtube.com/embed?listType=list&list=UUF-q9z4IF8CUWyj1_8hZDtg&autoplay=1"
+      />
+    </Playlist>
+    <Content>
+      <h1>Lets be sad together.</h1>
+    </Content>
   </Layout>
 );
