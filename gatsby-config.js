612ed2a9e36c77402f3ca8d946570b53fc4f8aa0
AriaFallah <aria@fallah.us>
2018-12-16 01:14:42 -0800

    add gtag analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,18 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: "sadlilblackboy",
-        short_name: "slbb",
+        short_name: "sadlilblackboy",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
         display: "minimal-ui",
         icon: "src/assets/logo.png"
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["UA-79031036-3"]
       }
     },
     "gatsby-plugin-offline"
