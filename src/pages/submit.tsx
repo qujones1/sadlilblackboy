31aac5a04eeb9e349dc201b6ee0d996fbdb44e1e
qujones1 <qujones1@umbc.edu>
2019-12-13 11:32:15 -0500

    completed construction of playlist submission page, and contact page

---
+++ src/pages/submit.tsx
@@ -0,0 +1,109 @@
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import React from "react";
+import styled from "react-emotion";
+import { Layout } from "../components/layout";
+
+const appleSvg = require("../assets/apple.svg") as string;
+const spotifySvg = require("../assets/spotify.svg") as string;
+const googlePng = require("../assets/googlePlay.png") as string;
+const amazonPng = require("../assets/amazon.png") as string;
+const soundcloudSvg = require("../assets/soundcloud2.png") as string;
+
+const Page = styled.div`
+  display: flex;
+  max-width: 600px;
+  margin: 0 auto;
+  flex-direction: column;
+  font-family: "Helvetica Neue Condensed";
+  color: white;
+
+  @media (max-width: 400px) {
+    flex-wrap: wrap;
+  }
+
+`;
+
+const Sub = styled.div `
+  display: inline-block;
+  box-sizing: border-box;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,1);
+  border-radius: 8px;
+  padding: 4px 16px;
+
+
+  p {
+    margin-top: 2px;
+    margin-bottom: 25px;
+  }
+
+  h1 {
+    margin-bottom: 5px
+  }
+
+  input {
+    width: 90%;
+    height: 35px;
+  }
+
+  button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 6px;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+  a:link {
+    text-decoration: none;
+    color: red;
+  }
+
+  form input[required=required]:after,
+  form select[required=required]:after,
+  form textarea[required=required]:after
+  {
+      color: red;
+      content: " *";
+  }
+
+  label {
+    display: block;
+    padding: 5px;
+    margin-bottom: 1px;
+  }
+`;
+export default ({ data }) => (
+  <Layout>
+    <Page>
+      <h1>[submit]</h1>
+      <Sub>
+        <p>
+          Fill in the form below if you are looking for a chance to be added to one of our spotify playlists.
+        </p>
+        <form name="contact" method="POST" data-netlify="true" action="#Success">
+          <div>
+            <label for="track">Spotify url:</label>
+            <input type="text" id="track" name="trackurl" placeholder="https://open.spotify.com/track/2xbAVLAt0sAyi5izoOjVVu" required/>
+            <p>
+              a link to one of your songs that you think will fit any of our playlists.
+            </p>
+          </div>
+          <div>
+            <label for="email" >Email:</label>
+            <input type="text" id="email" name="email" placeholder="name@domain.com" />
+            <p>
+              Provide your email so that we could reach out to you.
+            </p>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </Sub>
+    </Page>
+  </Layout>
+);
