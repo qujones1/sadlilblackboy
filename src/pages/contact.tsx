d2f58528f49d4fccfae3beb58dc10f6fe0c51e12
Aria Fallah <aria@fallah.us>
2020-01-20 21:23:51 -0800

    Refactor contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,117 +9,74 @@ const Page = styled.div`
   flex-direction: column;
   font-family: "Helvetica Neue Condensed";
   color: white;
-
-  @media (max-width: 400px) {
-    flex-wrap: wrap;
-  }
+  padding: 16px 8px;
 `;
 
-const Sub = styled.div`
-  display: inline-block;
-  box-sizing: border-box;
+const FormContainer = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
   border-radius: 8px;
-  padding: 4px 16px;
+  padding: 12px;
 
-  p {
-    margin-top: 2px;
-    margin-bottom: 25px;
-  }
-
-  h1 {
-    margin-bottom: 5px;
+  input,
+  textarea {
+    width: 90%;
+    margin-top: 4px;
+    margin-bottom: 12px;
+    font-size: 14px;
   }
 
   input {
-    width: 90%;
     height: 35px;
   }
 
-  button {
-    background-color: #4caf50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    border-radius: 6px;
-    font-size: 16px;
-    margin-bottom: 10px;
-  }
-
-  input {
-    margin-top: 5px;
-  }
-
-  a:link {
-    text-decoration: none;
-    color: red;
-  }
-
-  form input[required="required"]:after,
-  form select[required="required"]:after,
-  form textarea[required="required"]:after {
-    color: red;
-    content: " *";
+  textarea {
+    height: 100px;
   }
 
   label {
     display: block;
-    padding: 5px;
-    margin-bottom: 1px;
-  }
-
-  textarea {
-    margin-bottom: 15px;
   }
 `;
 
-export default ({ data }) => (
+const SubmitButton = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 12px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 6px;
+  font-size: 16px;
+`;
+
+export default () => (
   <Layout>
     <Page>
       <h1>[contact]</h1>
-      <Sub>
+      <FormContainer>
         <form
           name="contact"
           method="POST"
           data-netlify="true"
           action="/success"
         >
+          <label>Name:</label>
+          <input type="text" name="name" placeholder="Rick Sanchez" required />
+          <label>Email:</label>
+          <input type="text" name="email" placeholder="name@domain.com" />
+          <label>Message:</label>
+          <textarea
+            name="message"
+            placeholder="Tell me how you feel..."
+            required
+          />
           <div>
-            <label>
-              Name:<br></br>
-              <input
-                type="text"
-                name="name"
-                placeholder="Rick Sanchez"
-                required
-              />
-            </label>
-            <p></p>
+            <SubmitButton type="submit">Send</SubmitButton>
           </div>
-          <div>
-            <label>
-              Email:<br></br>
-              <input type="text" name="email" placeholder="name@domain.com" />
-            </label>
-            <p></p>
-          </div>
-          <div>
-            <label>
-              Message:<br></br>
-              <textarea
-                name="message"
-                placeholder="Tell me how you feel . . ."
-                required
-                style={{ width: "80%", height: "100px" }}
-              />
-            </label>
-          </div>
-          <button type="submit">Send</button>
+          <input type="hidden" name="form-name" value="contact" />
         </form>
-      </Sub>
+      </FormContainer>
     </Page>
   </Layout>
 );
