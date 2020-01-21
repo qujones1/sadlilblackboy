26d05321878a12e1afcaf840f952a1c398d8398a
Aria Fallah <aria@fallah.us>
2020-01-20 21:38:58 -0800

    Refactor submissions page

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -17,75 +17,49 @@ const Page = styled.div`
   flex-direction: column;
   font-family: "Helvetica Neue Condensed";
   color: white;
-
-  @media (max-width: 400px) {
-    flex-wrap: wrap;
-  }
+  padding: 16px 8px;
 `;
 
-const Sub = styled.div`
-  display: inline-block;
-  box-sizing: border-box;
+const FormContainer = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
   border-radius: 8px;
-  padding: 4px 16px;
-
-  p {
-    margin-top: 2px;
-    margin-bottom: 25px;
-    margin-left: 5px;
-  }
-
-  h1 {
-    margin-bottom: 5px;
-  }
+  padding: 12px;
 
   input {
-    margin-top: 5px;
     width: 90%;
+    margin-top: 4px;
+    margin-bottom: 12px;
+    font-size: 14px;
     height: 35px;
-  }
-
-  button {
-    background-color: #4caf50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    border-radius: 6px;
-    font-size: 16px;
-    margin-bottom: 10px;
-  }
-
-  a:link {
-    text-decoration: none;
-    color: red;
-  }
-
-  form input[required="required"]:after,
-  form select[required="required"]:after,
-  form textarea[required="required"]:after {
-    color: red;
-    content: " *";
   }
 
   label {
     display: block;
-    padding: 5px;
-    margin-bottom: 1px;
   }
+`;
+
+const SubmitButton = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 12px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 6px;
+  font-size: 16px;
 `;
 
 export default ({ data }) => (
   <Layout>
     <Page>
       <h1>[submit]</h1>
-      <Sub>
+      <FormContainer>
         <p>
           Fill in the form below if you are looking for a chance to be added to
-          one of our spotify playlists.
+          one of our spotify playlists. Provide a link to one of your songs that
+          you think will fit any of our playlists, and provide your email so
+          that we could reach out to you.
         </p>
         <form
           name="submissions"
@@ -93,31 +67,23 @@ export default ({ data }) => (
           data-netlify="true"
           action="/success"
         >
+          <label>Spotify URL:</label>
+          <input
+            type="text"
+            name="trackurl"
+            placeholder="https://open.spotify.com/track/2xbAVLAt0sAyi5izoOjVVu"
+            required
+          />
+
+          <label>Email:</label>
+          <input type="text" name="email" placeholder="name@domain.com" />
+
           <div>
-            <label>
-              Spotify url:<br></br>
-              <input
-                type="text"
-                name="trackurl"
-                placeholder="https://open.spotify.com/track/2xbAVLAt0sAyi5izoOjVVu"
-                required
-              />
-            </label>
-            <p>
-              Provide a link to one of your songs that you think will fit any of
-              our playlists.
-            </p>
+            <SubmitButton type="submit">Submit</SubmitButton>
           </div>
-          <div>
-            <label>
-              Email:<br></br>
-              <input type="text" name="email" placeholder="name@domain.com" />
-            </label>
-            <p>Provide your email so that we could reach out to you.</p>
-          </div>
-          <button type="submit">Submit</button>
+          <input type="hidden" name="form-name" value="submissions" />
         </form>
-      </Sub>
+      </FormContainer>
     </Page>
   </Layout>
 );
