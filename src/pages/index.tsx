8bdf5ca5a08f012a3e831e1d2ed6ff69aa40fb76
qujones1 <qujones1@umbc.edu>
2020-07-30 14:34:30 -0400

    added kaleidoscope

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -204,7 +204,7 @@ export default ({ data }) => (
       </Body>
       <Body>
         <ReleaseFrame>
-          <ImgContainer href="https://distrokid.com/hyperfollow/sadlilblackboy/carnival">
+          <ImgContainer href="https://distrokid.com/hyperfollow/sadlilblackboy/kaleidoscope-feat-nuq">
             <Img fluid={data.singleRelease.childImageSharp.fluid} />
           </ImgContainer>
         </ReleaseFrame>
@@ -266,7 +266,7 @@ export const query = graphql`
         }
       }
     }
-    singleRelease: file(relativePath: { eq: "CarnivalCover.PNG" }) {
+    singleRelease: file(relativePath: { eq: "kaliedoscope.PNG" }) {
       childImageSharp {
         fluid(maxWidth: 1024) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
