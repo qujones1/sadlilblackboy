dc3cb97618e0d494940ce974a01063dddd598907
qujones1 <qujones1@umbc.edu>
2020-11-05 16:14:17 -0500

    added spirits links in cover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -209,7 +209,7 @@ export default ({ data }) => (
           </ImgContainer>
         </ReleaseFrame>
         <ReleaseFrame>
-          <ImgContainer href="https://distrokid.com/hyperfollow/nuq/say-never">
+          <ImgContainer href="https://distrokid.com/hyperfollow/nuq/spirits">
             <Img fluid={data.beatRelease.childImageSharp.fluid} />
           </ImgContainer>
         </ReleaseFrame>
@@ -259,7 +259,7 @@ export default ({ data }) => (
 
 export const query = graphql`
   {
-    beatRelease: file(relativePath: { eq: "saynever.jpg" }) {
+    beatRelease: file(relativePath: { eq: "spirits.PNG" }) {
       childImageSharp {
         fluid(maxWidth: 1024) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
