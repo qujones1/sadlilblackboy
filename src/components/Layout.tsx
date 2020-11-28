b57f602761b50101ba49f5b29b75070dc273fb4a
Aria Fallah <aria@fallah.us>
2020-11-27 23:52:38 -0500

    Add favicon files

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,12 +33,34 @@ export function Layout({ children, className, header }: Props) {
           {
             name: "description",
             content:
-              "Who are you? Why are you here? Lofi hiphop, music, anime, and chill vibes. Lets all be sad together.",
-          },
+              "Who are you? Why are you here? Lofi hiphop, music, anime, and chill vibes. Lets all be sad together."
+          }
         ]}
       >
         <html lang="en" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#b91d47" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
+
       <Nav />
 
       <main className={`max-w-screen-md mx-auto p-2 ${className}`}>
