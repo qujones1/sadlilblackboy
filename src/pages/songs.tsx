cc077d8b2b35d16e30afbce9c044e87f27db599b
qujones1 <qxjonesii@gmail.com>
2022-10-09 17:51:02 -0400

    started CMS backend and front end designs

---
+++ src/pages/songs.tsx
@@ -0,0 +1,14 @@
+import { graphql } from "gatsby";
+import Image from "gatsby-image";
+import React from "react";
+import { Layout } from "../components/Layout";
+
+
+
+export default ({ data }) => (
+  <Layout header="songs">
+    <div className="grid grid-cols-2 sm:grid-cols-1 gap-x-1 gap-y-5">
+    
+    </div>
+  </Layout>
+);
