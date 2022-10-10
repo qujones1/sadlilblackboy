c90e93551d17ddcd38b404ff60954b4d711151c4
qujones1 <qxjonesii@gmail.com>
2022-10-10 10:14:27 -0400

    tested new style changes

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -7,35 +7,39 @@ const staff = [
   {
     imgName: "Quentin",
     name: "Quentin",
+    social: "@Sadlillblackboy",
     bio:
       "Multi-instrumentalist and vocalist Quentin Jones leads the Alternative Hip-Hop and Pop project, Sadlillblackboy (also known as producer nuq). Based in Washington, D.C."
   },
   {
     imgName: "Doni",
     name: "Doni",
+    social:"@sparklicious",
     bio:
       "'OMFG is this hentai?' - Doni",
   },
   {
     imgName: "Aria",
     name: "Aria",
+    social: "@m0meni",
     bio:
       "'I was the first person to listen to Jack Harlow' - Aria",
   },
 ];
 
 export default ({ data }) => (
-  <Layout header="staff">
-    <div className="grid grid-cols-2 sm:grid-cols-1 gap-x-1 gap-y-5">
+  <Layout header="About Us">
+
+    <h1 class="has-text-align-center">Meet the Team</h1>
+    <div className="wp-container-4 wp-block-columns has-3-columns">
     {staff.map((staff) => (
-      <div className="no-borderRadiusImportant inline-block" style={{border: '1px solid black',borderRadius: '5px!important'}} key={staff.imgName} >
+      <div className="wp-block-columns has-2-columns" key={staff.imgName} >
         <div className="inline-block">
-        <Image className="rounded" fluid={data[staff.imgName].childImageSharp.fluid} />
-        {staff.name}
+          <Image className="rounded" fluid={data[staff.imgName].childImageSharp.fluid} />
+          <h3 class="has-text-align-center">{staff.name}</h3>
+          <h3 class="has-text-align-center">{staff.social}</h3>
         </div>
-        <div className="inline-block">
-        {staff.bio}
-        </div>
+        <div className="inline-block">{staff.bio}</div>
       </div>
     ))}
     </div>
